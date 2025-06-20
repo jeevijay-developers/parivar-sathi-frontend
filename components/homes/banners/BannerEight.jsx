56d0ba241fe87a72a1c2d2635a98a04a86c8f57d
iCodeLakshay <lakshaysaxena2217@gmail.com>
2025-06-20 16:26:06 +0530

    Enhance BannerEight and FeaturedTrips components with improved OPD camp handling and default image

--- components/homes/banners/BannerEight.jsx
+++ components/homes/banners/BannerEight.jsx
@@ -9,7 +9,7 @@ import { useEffect, useState } from "react";
 export default function BannerEight() {
   const [opdCamp, setOpdCamp] = useState({});
 
-  useEffect(() =>{
+  useEffect(() => {
     const fetchOpdCamp = async () => {
       try {
         const response = await axiosInstance.get("/opds/opdcampsfour");
@@ -19,7 +19,7 @@ export default function BannerEight() {
         console.error("Error fetching OPD camp:", error);
       }
     };
-  
+
     fetchOpdCamp();
   }, []);
   return (
@@ -29,7 +29,7 @@ export default function BannerEight() {
 
         <div className="cta__image">
           <img
-            src={opdCamp?.image}
+            src={opdCamp?.image || '/img/cta/7/default.jpg'}
             width={750}
             height={600}
             alt="image"
@@ -53,19 +53,34 @@ export default function BannerEight() {
         <div className="row">
           <div className="col-xxl-4 col-xl-5 col-lg-6 col-md-7">
             <div className="cta__content">
-              <h2
-                data-aos="fade-up"
-                data-aos-delay=""
-                className="text-40 text-accent-3 md:text-30  lh-13"
-              >
-                Register to our next{" "}
-                OPD Camp in{" "}
-                <br className="lg:d-none" />
-                <span className="text-accent-2">{opdCamp?.location} </span>
-                <br className="lg:d-none" />
-                Destination on <br className="lg:d-none" />
-                <span className="text-accent-2">{new Date(opdCamp?.date).toLocaleDateString()}</span>
-              </h2>
+              {!(opdCamp?.location && opdCamp.date) ?
+                (
+                  <h2
+                    data-aos="fade-up"
+                    data-aos-delay=""
+                    className="text-40 text-accent-3 md:text-30  lh-13"
+                  >
+                    Our next OPD Camp coming Soon...
+                  </h2>
+                ) : (
+                  <h2
+                    data-aos="fade-up"
+                    data-aos-delay=""
+                    className="text-40 text-accent-3 md:text-30  lh-13"
+                  >
+                    Register to our next{" "}
+                    OPD Camp in{" "}
+                    <br className="lg:d-none" />
+                    <span className="text-accent-2">{opdCamp?.location} </span>
+                    <br className="lg:d-none" />
+                    Destination on <br className="lg:d-none" />
+                    <span className="text-accent-2">{new Date(opdCamp?.date).toLocaleDateString()}</span>
+                  </h2>
+                )
+
+              }
+
+
 
               {/* <p data-aos="fade-up" data-aos-delay="" className="mt-10"> */}
               {/* <div className="d-flex justify-between items-center border-1-top text-13 text-dark-1 pt-10 mt-10">
@@ -80,19 +95,19 @@ export default function BannerEight() {
                 </div>
               </div> */}
               {/* </p> */}
-
+              {/* 
               <div
                 data-aos="fade-right"
                 data-aos-delay=""
                 className="mt-30 md:mt-20"
-              >
-                {/* <button className="button -md -dark-1 bg-accent-2 text-white">
+              > */}
+              {/* <button className="button -md -dark-1 bg-accent-2 text-white">
                   <Link href="/">
                     Register
                     <i className="icon-arrow-top-right ml-10 text-16"></i>
                   </Link>
                 </button> */}
-              </div>
+              {/* </div> */}
             </div>
           </div>
         </div>
