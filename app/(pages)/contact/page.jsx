4332166024226df7e94dc6f826e6de824ce4e870
iCodeLakshay <lakshaysaxena2217@gmail.com>
2025-07-26 14:38:38 +0530

    feat: Add ContactBanner component with WhatsApp integration and responsive styles

--- app/(pages)/contact/page.jsx
+++ app/(pages)/contact/page.jsx
@@ -1,10 +1,10 @@
+import ContactBanner from "@/components/contact/ContactBanner";
 import FooterOne from "@/components/layout/footers/FooterOne";
 import FooterThree from "@/components/layout/footers/FooterThree";
 import Header1 from "@/components/layout/header/Header1";
 import Header4 from "@/components/layout/header/Header4";
 import ContactForm from "@/components/pages/contact/ContactForm";
 import Locations from "@/components/pages/contact/Locations";
-import Map from "@/components/pages/contact/Map";
 import React from "react";
 
 export const metadata = {
@@ -17,10 +17,9 @@ export default function page() {
     <>
       <main>
         <Header4 />
-        <Map />
+        <ContactBanner />
         <Locations />
         <ContactForm />
-
         <FooterThree />
       </main>
     </>
